ecace4004b085e38f0d48fc5c9947368a7db9e10
e-n-p <nickellam0@gmail.com>
2024-04-30 11:36:16 +0200

    re-wrote grid construction to be more efficient/work

--- src/app/models/classes/cell.class.ts
+++ src/app/models/classes/cell.class.ts
@@ -1,21 +1,21 @@
 export class Cell {
-    
+
     //cell change operations here, more complex things well fleshed out.
     //comparisons?
     constructor(
-        private value:number
-    ) {}
+        private value: number = 0
+    ) { }
 
-    getValue():number {
+    getValue(): number {
         return this.value;
     }
-    setValue(newNum:number):void {
+    setValue(newNum: number): void {
         this.value = newNum;
     }
-    seed(){
+    seed() {
         this.value = 1;
     }
-    kill(){
+    kill() {
         this.value = 0;
     }
 }
--- src/app/models/classes/grid.class.ts
+++ src/app/models/classes/grid.class.ts
@@ -2,11 +2,22 @@ import { Cell } from "./cell.class"
 
 export class Grid {
 
-    private grid: Cell[][];
+    private grid!: Cell[][];
     //grid search and print operations in here
     constructor(private width: number, private height: number) {
-        this.grid = new Array(height).fill(false).map(() => new Array(width).fill(new Cell(0)));
-        this.seedGrid(1);
+        this.createGrid(height, width)
+        this.seedGrid(10);
+    }
+
+    createGrid(height: number, width: number) {
+        const outerArray = new Array(height);
+        for (let i = 0; i < height; i++) {
+            outerArray[i] = [];
+            for (let j = 0; j < width; j++) {
+                outerArray[i][j] = new Cell();
+            }
+        }
+        this.grid = outerArray;
     }
 
     getGridRepresentation() {
